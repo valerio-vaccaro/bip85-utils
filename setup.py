d14bd902fd425535ca172b80150bb71b7b3e0bcb
Valerio Vaccaro <valerio.vaccaro@gmail.com>
2024-05-19 21:13:09 +0200

    update requirements and publish version 0.0.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name = 'bip85-cli',
-    version = '0.0.1',
+    version = '0.0.2',
     author = 'Valerio Vaccaro',
     author_email = 'valerio.vaccaro@gmail.com',
     license = 'MIT',
